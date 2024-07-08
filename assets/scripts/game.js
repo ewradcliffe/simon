47446428e0ae3cb8bd35689fc7de044e4e6d5f7e
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-07-08 12:35:31 +0000

    Add basic testing.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,7 +1,11 @@
 let game = {
     currentGame: [],
     score: 0,
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
 };
 
 
-module.exports = { game };
+
+
+module.exports = { game};
